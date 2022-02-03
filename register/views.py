5b611c725bd7f35499cdb93a737c456cb5ddb89e
Ali <shahrokh.r1379@gmail.com>
2022-02-03 20:44:48 +0330

    create function register

--- register/views.py
+++ register/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import login
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(reqeust, 'Registration successful.')
+            return redirect('home')
+
+        messages.error(request, 'Unsuccessful registration.')
+
+    form = RegisterForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'registration/register.html', context)
